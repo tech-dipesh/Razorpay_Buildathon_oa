23563cdb925396d61adc2e148fe72fcf49af1121
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-09-06 21:55:20 +0530

    Add the More command script for here, with also a prisma config setup and package json

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,6 +1,7 @@
-
+import { config } from "dotenv";
+import {resolve} from "node:path"
 import { defineConfig, env } from "prisma/config";
-
+config({ path: resolve("../../.env") });
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
